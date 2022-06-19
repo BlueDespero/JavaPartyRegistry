e664dd7ae3b4a6b9d550f05f6b7cdff0c21190d2
BlueDespero <cezarytroska@gmail.com>
2022-06-19 22:12:48 +0200

    Remove unnecessary ids, add note property to the bundle

--- src/main/java/com/registry/partyregistry/controllers/RegistryController.java
+++ src/main/java/com/registry/partyregistry/controllers/RegistryController.java
@@ -1,4 +1,14 @@
 package com.registry.partyregistry.controllers;
 
+import javafx.scene.control.*;
+
 public class RegistryController {
+    public TextField party_name_input;
+    public DatePicker event_date_input;
+    public ChoiceBox category_input;
+    public TextArea notes_input;
+    public TextField budget_input;
+    public TextField starting_time_input;
+    public Button add_button;
+    public Label error_label;
 }
